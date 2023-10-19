5dac6761334627461cec771e4904d0c2ee0bf1c8
Gregz9 <gregorkajda@gmail.com>
2023-10-19 23:44:59 +0200

    Finished script for extraction of images, removed test file, and added pictures to github repo
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- src/area_extraction.py
+++ src/area_extraction.py
@@ -1,15 +1,18 @@
 import cv2 
 import numpy as np
 import os
+import copy
 
 drawing = False
 ix, iy = -1, -1
 extracted_areas = []
+image_path = os.path.dirname(os.path.dirname(__file__)) + "/data/Bilde1.png"
+img = cv2.imread(image_path)
+original_img = copy.deepcopy(img)
 
 # Mouse callback function
 def draw_rectangle(event, x, y, flags, param):
-    global ix, iy, drawing, extracted_areas
-    mode = True 
+    global ix, iy, drawing, extracted_areas, img, original_img
 
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
@@ -17,11 +20,24 @@ def draw_rectangle(event, x, y, flags, param):
     
     elif event == cv2.EVENT_LBUTTONUP:
         if drawing:
-            cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), 2)
-            extracted_area = img[iy:y, ix:x]  # Extract the marked area
+
+            
+            cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), 1)
+            if ix > x:
+                ix, x = x, ix
+            if iy > y:
+                iy, y = y, iy
+            extracted_area = img[iy+1:y-1, ix+1:x-1]  # Extract the marked area
             extracted_areas.append(extracted_area)
             drawing = False
+            original_img = copy.deepcopy(img)
 
+    elif event == cv2.EVENT_MOUSEMOVE: 
+        if drawing: 
+            temp_img = original_img.copy()
+            cv2.rectangle(temp_img, (ix, iy), (x, y), (0, 255, 0), 1)
+            img = temp_img.copy()
+    
 
 # Load the image
 image_path = os.path.dirname(os.path.dirname(__file__)) + "/data/Bilde1.png"
@@ -37,8 +53,10 @@ while True:
         break
 
 cv2.destroyAllWindows()
+for i, area in enumerate(extracted_areas):
+    print(area.shape)
+    cv2.imshow(f'Area {i}', area)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-# Convert the extracted areas to numpy arrays
-print(extracted_areas)
-      
 
