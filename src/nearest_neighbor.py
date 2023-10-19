3f691bf683e06b4a7a21de914b05d327d405999a
Gregz9 <gregorkajda@gmail.com>
2023-10-19 11:08:23 +0200

    Nearest neighbor classificator

---
+++ src/nearest_neighbor.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+def read_file(file_path):
+    data_array = np.loadtxt(file_path)
+    targets, obs = data_array[:, 0].copy(), data_array[:, 1:].copy()
+    return targets, obs
+
+
+def split_data(obs, targets):
+    train_obs, train_targets = obs[1::2], targets[1::2]
+    test_obs, test_targets = obs[0::2], targets[0::2]
+    return train_obs, test_obs, train_targets, test_targets
+
+
+def measure_dist(obs_1, obs_2):
+    distance = np.linalg.norm(obs_1 - obs_2)
+    return distance
+
+def nearest_neighbour(train_obs, train_targets):
+
+    c_train_obs = np.zeros((len(train_obs), 1))
+
+    for i in range(len(train_obs)):
+        
+        near_neigh = np.argmin([measure_dist(train_obs[i], train_obs[j]) for j in range(len(train_obs)) if i != j])
+        c_train_obs[i] = train_targets[near_neigh]
+
+    return c_train_obs
+
+
+    
+     
+    
+    
+
+if __name__ == "__main__": 
+    targets, obs = read_file("/home/gregz/Programs/Pattern-Recognition_1/data/ds-1.txt")
+    train_obs, test_obs, train_targets, test_targets = split_data(obs, targets)   
+
+    # measure_dist(train_obs[3], train_obs[1])
+    nearest_neighbour(train_obs, train_targets)
+
+
+
